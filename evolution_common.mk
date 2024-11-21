ebc437079faa2297ba24921973ac5e66573701fc
AnierinB <anierin@evolution-x.org>
2024-11-21 21:15:52 +0000

    zumapro: Initialize for Evolution X 10.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ evolution_common.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: 2024 The Evolution Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-evolution
+
+# PixelParts
+include packages/apps/PixelParts/device.mk
